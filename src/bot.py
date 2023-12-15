659dba4f599d45b89f7c2e1f7dca7c6d2d9481a6
jackcompton94 <jackcompton94@gmail.com>
2023-12-14 23:00:34 -0600

    modified prompting

--- src/bot.py
+++ src/bot.py
@@ -9,32 +9,37 @@ client = OpenAI(
 
 def ask_gpt(current_map, current_side, current_round, results, money, total_kills, total_deaths):
     prompt = f"""
+            Use the provided game data, your creativity, and the following steps to deliver Counter-Strike 2 strategies 
+            in order to give my team as much of an advantage as possible: 
+             
+             - The strategy is for the Current Round and Current Map only.
+             - Provide what positions to take according to the Current Side and Current Map.
+             - Provide what to buy based on how much Money I have and the Past Results.
+             - Provide the approach to be taken based on the positions you provided/
+             - Predict if the other team has the money to full-buy, full-save, or eco based on the Past Results.
+             - Predict how the other team may play the Current Round based on the Past Results.
+             - Deliver the response succinctly with minimal explanation. 
+            
+            ## Game Data
+            
             Current Map: {current_map}
             Current Side: {current_side}
             Current Round: {current_round}
             Past Results: {results}
             Money: {money}
-            Kills: {total_kills}
-            Deaths: {total_deaths}
+            My Kills: {total_kills}
+            My Deaths: {total_deaths} 
 
-            Provide a strategy for what positions to take, what to buy, and how we should approach this round only. 
-            In order to do this correctly, you must base your strategy off of what round we're in and the past results. 
+            ## Final Notes:
             
-            Predict if the other team has the money to full-buy, save, or eco. Also predict, what they may do based on 
-            where the game is at to give me in order to give me as much of an advantage as possible.
-
-            Key things to remember:
-            - If its the first round, or the first round on a new side we wont have money to buy rifles.
-            - There are only 5 players on each team.
-
-            Provide the strategy in very-short summary/bullet-point format.
-            Do not give generic advice or too much "fluff", just short and sweet.
+            - On the 1st and 13th rounds, teams switch sides, and the economy resets for both teams.
+            - There are only 5 players on a team.
             """
 
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "You are a shot-caller for Counterstrike 2."},
+            {"role": "system", "content": "You are a Counter-Strike 2 shot caller."},
             {"role": "user", "content": prompt}
         ],
         max_tokens=2000,
