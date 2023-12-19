b251c939606631f032897d30760ed1ac1899c7f9
jackcompton94 <jackcompton94@gmail.com>
2023-12-18 22:47:42 -0600

    implemented disconnect_with_steamid

--- main.py
+++ main.py
@@ -1,40 +1,46 @@
 import os
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_socketio import join_room
 from flask_cors import CORS
 from src.game_handler import handle_game_state_update
 from config.socket_manager import socketio
 
 app = Flask(__name__)
-CORS(app, resources={r"/socket.io/*": {"origins": "http://localhost:*"}})  # Specify the allowed origin
+CORS(app, resources={r"/socket.io/*": {"origins": "http://localhost:*"}})
 socketio.init_app(app)
+connected_steamids = set()
 
 
 @app.route('/update_gsi', methods=['POST'])
 def update_gsi():
-    return handle_game_state_update(request)
+    data = request.get_json()
+    steamid = data.get('steamid')
 
-
-@socketio.on('connect')
-def handle_connect():
-    print("User connected")
+    if steamid in connected_steamids:
+        return handle_game_state_update(request)
+    else:
+        response = {'status': 'SteamID not connected', 'message': 'POST received but user not connected to AIGL'}
+        print(response['message'])
+        return jsonify(response)
 
 
 @socketio.on('connect_with_steamid')
 def handle_connect_with_steamid(data):
     steamid = data.get('steamid')
     join_room(steamid)
+    connected_steamids.add(steamid)
     print(f"User connected with SteamID: {steamid}")
     socketio.emit('connected', {'message': 'Successfully connected'}, room=steamid)
 
 
-@socketio.on('disconnect')
-def handle_disconnect():
-    print(f"User disconnected")
+@socketio.on('disconnect_with_steamid')
+def handle_disconnect_with_steamid(data):
+    # Extract the SteamID from the disconnect event
+    steamid = data.get('steamid')
+    connected_steamids.discard(steamid)
+    print(f"User disconnected with SteamID: {steamid}")
 
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8888))
     socketio.run(app, port=port)
-
-
--- src/phase.py
+++ src/phase.py
@@ -7,16 +7,16 @@ def handle_phase(game_data, steamid):
     current_phase = game_data['round_phase']
 
     if steamid in phases and phases[steamid] == current_phase:
-        return None  # No action needed if the phase hasn't changed
+        # No action needed if the phase hasn't changed
+        return None
     else:
         phases[steamid] = current_phase
 
         if current_phase == 'freezetime':
             # During freezetime, return the result of the ask_gpt function
-            return ask_gpt(game_data['map_name'], game_data['current_side'], game_data['current_round'], game_data['round_wins'], game_data['money'], game_data['kills'], game_data['deaths'])
+            # return ask_gpt(game_data['map_name'], game_data['current_side'], game_data['current_round'], game_data['round_wins'], game_data['money'], game_data['kills'], game_data['deaths'])
+            return f'freezetime'
         elif current_phase == 'live':
-            # During live phase, return a keyword or code indicating the phase
             return f'live'
         elif current_phase == 'over':
-            # During over phase, return a keyword or code indicating the phase
             return f'over'
